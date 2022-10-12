9ca71919d7abbee1b110238a7c252b55c9567cd7
Seiichi-Matsui <s-matsui@zima-corp.com>
2022-10-12 19:10:24 +0900

    デプロイ修正

--- src/app/form-area/form-area.module.ts
+++ src/app/form-area/form-area.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { SituationListComponent } from './situation/situation-list/situation-list.component';
 import { SituationComponent } from './situation/situation.component';
 import { FormPageComponent } from './form-page/form-page.component';
 import { FormAreaComponent } from './form-area.component';
@@ -9,7 +10,6 @@ import { FormMainComponent } from './form-page/form-main/form-main.component';
 import { FormNavComponent } from './form-page/form-nav/form-nav.component';
 import { FormSettingComponent } from './form-page/form-setting/form-setting.component';
 import { FormComponent } from './situation/form/form.component';
-import { SituationListComponent } from './situation/situation-list/situation-list.component';
 import { FormListComponent } from './situation/form/form-list/form-list.component';
 import { FormsModule } from '@angular/forms';
 import { PagesComponent } from './pages/pages.component';
